ec7e065b51113fe498b684ddbb9ebb21462332f4
Lissette Valle <Lissettev.valle@gmail.com>
2023-11-15 14:42:35 -0700

    showcount added to bands model

--- models/Band.js
+++ models/Band.js
@@ -4,7 +4,8 @@ const {Sequelize, sequelize} = require('../db');
 // TODO - define the Band model
 let Band = sequelize.define("band", {
     name: DataTypes.STRING,
-    genre: DataTypes.STRING
+    genre: DataTypes.STRING,
+    showCount: DataTypes.INTEGER
 });
 
 module.exports = {
